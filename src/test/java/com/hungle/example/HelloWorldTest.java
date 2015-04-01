918140e2c849dda37c0b3dcf41baff02ec061998
hung-le <hle999@gmail.com>
2015-04-01 13:01:31 -0700

    remove extra dirs

---
+++ src/test/java/com/hungle/example/HelloWorldTest.java
@@ -0,0 +1,21 @@
+package com.hungle.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.hungle.example.test01.HelloWorld;
+
+public class HelloWorldTest {
+
+    @Test
+    public void test1() {
+        String message = "Hello World!";
+        
+        HelloWorld hello = new HelloWorld(message);
+        
+        String msg = hello.getMessage();
+        Assert.assertNotNull(msg);
+        Assert.assertTrue(msg.length() > 0);
+        Assert.assertEquals(message, msg);        
+    }
+}
